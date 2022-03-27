b475d02f17b0ab67e911a150b0c44490d47add7d
David Hidalgo <dhidalgo@nalandaglobal.com>
2022-03-27 21:23:48 +0200

    guardados cambios

--- src/controllers/torneos.controller.js
+++ src/controllers/torneos.controller.js
@@ -1,7 +1,12 @@
 import Torneo from '../models/Torneo'
 
-export const createTorneo = async (req, res) => {  
+export const createTorneo = async (req, res) => { 
+    const {nombre, lugar, fecha, numCampos} = req.body;
 
+    const newTorneo = new Torneo({nombre, lugar, fecha, numCampos})
+    const TorneoGuardado = await newTorneo.save()
+
+    res.json(TorneoGuardado); 
 }
 
 export const getTorneos = async (req, res) => {
@@ -15,9 +20,18 @@ export const getTorneoById = async (req, res) => {
 }
 
 export const updateTorneoById = async (req, res) => {
+    const { id } = req.params;
+    const {nombre, lugar, fecha, numCampos} = req.body;
+    const torneoActualizado = await Torneo.findByIdAndUpdate(id, {nombre, lugar, fecha, numCampos}, {
+        new:true
+    });
 
+    res.json(torneoActualizado);
 }
 
 export const deleteTorneoById = async (req, res) => {
+    const { id } = req.params;
+    const torneoEliminado = await Torneo.findByIdAndDelete(id);
 
+    res.json(torneoEliminado);
 }
